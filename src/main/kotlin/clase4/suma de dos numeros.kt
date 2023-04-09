355bbf1bc7859f5b013d61b7da8c8003c6daa552
jirma <jirman.rodriguez@uptc.edu.co>
2023-04-09 13:34:58 -0500

    funciones

---
+++ src/main/kotlin/clase4/suma de dos numeros.kt
@@ -0,0 +1,9 @@
+package clase4
+
+fun main (){
+    println(sumar(50,25))
+
+}
+fun sumar(num1:Int,num2:Int):Int {
+    return num1 + num2
+}
---
+++ src/main/kotlin/clase4/when,function,if-else,cycles.kt
@@ -0,0 +1,20 @@
+package clase4
+
+fun main (){
+    /**
+     * day 4
+     * - when , function , if else , cycles
+     * - program kitchen
+     *
+      */
+ // vamos a llamar la fruta
+    milkshake("banana")
+    milkshake("strawberry")
+    milkshake("kiwi")
+}
+fun milkshake (fruit:String){  // batido , funcion.
+    println("starting a milkshake ")
+    println("_________________________")
+    println("the milkshake of $fruit is done")
+
+}
