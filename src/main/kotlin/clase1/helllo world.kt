1b057e15aa0d8ab05a6ea2c755abd432097f9a5e
jirma <jirman.rodriguez@uptc.edu.co>
2023-04-09 11:21:16 -0500

    clase 2

---
+++ src/main/kotlin/clase1/helllo world.kt
@@ -0,0 +1,26 @@
+package clase1
+
+fun main (){
+    println(" hola mundo")
+
+    /**
+     * variables:
+     * var: se representan variables que se pueden cambiar.
+     * val: no se pueden cambiar
+     */
+
+    val size =10 // Int / Integer
+    println(size)
+
+    var temperature= 30.5 // float
+    temperature = 36.0
+    temperature = 37.0
+    temperature= 38.0
+    temperature=40.0
+    println(temperature)
+
+    var turn_on=true // boolean
+    println(turn_on)
+
+
+}
